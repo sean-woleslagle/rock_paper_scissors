401a2950a6f498127a830860940c596ba904e32c
Sean Woleslagle <jacx92@gmail.com>
2020-01-21 23:18:39 -0500

    Updated functions

---
+++ rps.js
@@ -0,0 +1,9 @@
+function computerPlay() {
+    let choices = ['Rock', 'Paper', 'Scissors'];
+    let randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    return randomChoice;
+}
+
+function gameRound(playerSelection, computerSelection) {
+    let choices = ['Rock', 'Paper', 'Scissors'];
+}
