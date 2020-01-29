1f25eafb8da8efaf4921f187750f66ed828591be
Sean Woleslagle <jacx92@gmail.com>
2020-01-29 15:37:10 -0500

    Got HTML and CSS to work together. Working on JS to make it functional.

--- rps.js
+++ rps.js
@@ -1,12 +1,36 @@
-let choices = ['Rock', 'Paper', 'Scissors'];
+const playerScore = 0;
+const computerScore = 0;
+const playerScore_span = document.getElementById("player-score"); //for player-score span tag in HTML
+const computerScore_span = document.getElementById("computer-score"); //for computer-score span tag in HTML
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
 
-function computerPlay() {
-    let randomChoice = choices[Math.floor(Math.random() * choices.length)];
-    return randomChoice;
+function getComputerChoice() {
+    const choices = ["r", "p", "s"];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
 }
 
-function playRound(playerSelection, computerSelection) {
-    for (choice in choices) {
-        if 
+function game(playerChoice) {
+    const computerChoice = getComputerChoice();
+    switch (playerChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            win();
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            lose();
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            tie();
+            break;
     }
 }
